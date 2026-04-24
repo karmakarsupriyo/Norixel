ba0502ed02bd0d8ceae7eb62e6311eb39ff5dc1a
karmakarsupriyo <karmakarsupriyo74@gmail.com>
2026-04-25 00:04:49 +0530

    Add files via upload

--- src/src/lib/utils.ts
+++ src/src/lib/utils.ts
@@ -1,1 +1,6 @@
-export const cn = (...classes: string[]) => classes.join(" ");
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
